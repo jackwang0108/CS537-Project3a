7f18d41226300b3e81a92d855117cc3e1134c68a
Jack <jack4shihong@gmail.com>
2022-10-27 22:51:03 -0500

    add build tools

---
+++ autobuild.sh
@@ -0,0 +1,12 @@
+shell_folder=$(cd "$(dirname "$0")" || exit; pwd)
+
+if [ ! -d "$shell_folder"/build ]; then
+   echo '\033[32mCreating build...\033[0m'
+   mkdir "$shell_folder"/build
+fi
+cd "$shell_folder"/build || exit
+
+echo '\033[32mBuilding...\033[0m'
+cmake ..
+make
+echo '\033[32mFinished building, everthing up-to-date...\033[0m'
