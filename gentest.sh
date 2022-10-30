8249984e782f4a7c9448cd1453bf489e612a959a
Jack <jack4shihong@gmail.com>
2022-10-30 04:08:24 -0500

    upgrade auto generation shell

--- gentest.sh
+++ gentest.sh
@@ -1,1 +1,5 @@
-dd if=/dev/random of=test.bin bs=1000 count=1 iflag=fullblock
+if [ $# -eq 1 ]; then 
+    dd if=/dev/random of=test.bin bs="$1" count=1 iflag=fullblock
+else
+    dd if=/dev/random of=test.bin bs=1000 count=1 iflag=fullblock
+fi
